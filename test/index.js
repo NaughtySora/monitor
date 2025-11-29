6ef4834117e197c8ad43dce386c1a8dc7672883b
Trofymenko Vladyslav <naughtysora@proton.me>
2025-11-30 00:23:47 +0200

    resource manager network proxies

--- test/index.js
+++ test/index.js
@@ -3,6 +3,8 @@
 const path = require("node:path");
 const fs = require("node:fs");
 
+const keep = ['resourceManager'];
+
 for (const test of fs.readdirSync(__dirname)) {
   require(path.resolve(__dirname, test));
 }
